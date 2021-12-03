9595d4586b75f6c2ff034734f7e64635c0d5f8ff
Matthew Chao <matthew.chao@coxautoinc.com>
2021-12-03 16:22:55 -0500

    day 3

---
+++ src/main/scala/chao/matthew/solutions/day01/Solution.scala
@@ -0,0 +1,11 @@
+package chao.matthew.solutions.day01
+
+object Solution {
+  @main
+  def solve(): Unit = {
+    scala.io.Source.fromFile(
+      "/Users/matthew.chao/practice/aoc2021/src/main/scala/chao/matthew/solutions/day01/input.txt"
+    )
+    println("hi")
+  }
+}
